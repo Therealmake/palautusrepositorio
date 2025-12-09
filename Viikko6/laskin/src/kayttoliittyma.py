885f32d0bba25aa4126bcf110aec20b778a2ad68
Therealmake <hemmimarkus1@gmail.com>
2025-12-09 19:45:21 +0200

    Viikko 6 tehtävät done

---
+++ Viikko6/laskin/src/kayttoliittyma.py
@@ -0,0 +1,101 @@
+from enum import Enum
+from tkinter import ttk, constants, StringVar
+
+
+class Komento(Enum):
+    SUMMA = 1
+    EROTUS = 2
+    NOLLAUS = 3
+    KUMOA = 4
+
+
+class Kayttoliittyma:
+    def __init__(self, sovelluslogiikka, root):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._root = root
+
+        self._komennot = {
+            Komento.SUMMA: self._komento_summa,
+            Komento.EROTUS: self._komento_erotus,
+            Komento.NOLLAUS: self._komento_nollaus,
+            Komento.KUMOA: self._komento_kumoa,
+        }
+
+        self._historia = []
+
+    def _komento_summa(self, arvo):
+        self._sovelluslogiikka.plus(arvo)
+
+    def _komento_erotus(self, arvo):
+        self._sovelluslogiikka.miinus(arvo)
+
+    def _komento_nollaus(self, arvo):
+        self._sovelluslogiikka.nollaa()
+
+    def _komento_kumoa(self, arvo):
+        if self._historia:
+            previous = self._historia.pop()
+            self._sovelluslogiikka.aseta_arvo(previous)
+
+    def kaynnista(self):
+        self._arvo_var = StringVar()
+        self._arvo_var.set(self._sovelluslogiikka.arvo())
+        self._syote_kentta = ttk.Entry(master=self._root)
+
+        tulos_teksti = ttk.Label(textvariable=self._arvo_var)
+
+        summa_painike = ttk.Button(
+            master=self._root,
+            text="Summa",
+            command=lambda: self._suorita_komento(Komento.SUMMA)
+        )
+
+        erotus_painike = ttk.Button(
+            master=self._root,
+            text="Erotus",
+            command=lambda: self._suorita_komento(Komento.EROTUS)
+        )
+
+        self._nollaus_painike = ttk.Button(
+            master=self._root,
+            text="Nollaus",
+            state=constants.DISABLED,
+            command=lambda: self._suorita_komento(Komento.NOLLAUS)
+        )
+
+        self._kumoa_painike = ttk.Button(
+            master=self._root,
+            text="Kumoa",
+            state=constants.DISABLED,
+            command=lambda: self._suorita_komento(Komento.KUMOA)
+        )
+
+        tulos_teksti.grid(columnspan=4)
+        self._syote_kentta.grid(columnspan=4, sticky=(constants.E, constants.W))
+        summa_painike.grid(row=2, column=0)
+        erotus_painike.grid(row=2, column=1)
+        self._nollaus_painike.grid(row=2, column=2)
+        self._kumoa_painike.grid(row=2, column=3)
+
+    def _suorita_komento(self, komento):
+        try:
+            arvo = int(self._syote_kentta.get())
+        except Exception:
+            arvo = 0
+
+        if komento != Komento.KUMOA:
+            self._historia.append(self._sovelluslogiikka.arvo())
+
+        action = self._komennot.get(komento)
+        if action:
+            action(arvo)
+
+        self._kumoa_painike["state"] = constants.NORMAL
+
+        if self._sovelluslogiikka.arvo() == 0:
+            self._nollaus_painike["state"] = constants.DISABLED
+        else:
+            self._nollaus_painike["state"] = constants.NORMAL
+
+        self._syote_kentta.delete(0, constants.END)
+        self._arvo_var.set(self._sovelluslogiikka.arvo())
---
+++ Viikko6/query-language/src/matchers.py
@@ -0,0 +1,79 @@
+class And:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if not matcher.test(player):
+                return False
+
+        return True
+
+
+class PlaysIn:
+    def __init__(self, team):
+        self._team = team
+
+    def test(self, player):
+        return player.team == self._team
+
+
+class HasAtLeast:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value >= self._value
+
+class All:
+    def test(self, player):
+        return True
+
+class Not:
+    def __init__(self, matcher):
+        self._matcher = matcher
+
+    def test(self, player):
+        return not self._matcher.test(player)
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+        return player_value < self._value
+
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+        return False
+
+class QueryBuilder:
+    def __init__(self, matcher=All()):
+        self._matcher = matcher
+
+    def playsIn(self, team):
+        return QueryBuilder(And(self._matcher, PlaysIn(team)))
+
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(self._matcher, HasAtLeast(value, attr)))
+
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(self._matcher, HasFewerThan(value, attr)))
+
+    def oneOf(self, *queries):
+        matchers = [q.build() for q in queries]
+        return QueryBuilder(Or(*matchers))
+
+    def build(self):
+        return self._matcher
